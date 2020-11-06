e5fdcb2a0008a3dbe168a2f54ed08f7363ebeaa4
Bhura Malik <rnd5@apaulinstruments.in>
2020-11-06 12:26:59 +0530

    siteload

---
+++ index.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta content="initial-scale=1,maximum-scale-1" name="viewport" />
+  <script data-ad-client="ca-pub-8795837294797076" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <title>Code Sack - Home</title>
+</head>
+
+<body>
+  <header class="web-header">
+    <nav class="header-container">
+
+      <div class="web-title">
+        <span class="web-icon"><img src="icon/logo.png"></span>
+        <span class="web-name-1"> <a href="#">Code </a> </span>
+        <span class="web-name-2"> <a href="#">Sack </a></span>
+      </div>
+
+      <div class="web-topics">
+        <span class="web-topic-1"> <a href="soc/">SOC </a> </span>
+        <span class="web-topic-2"> <a href="#">RTOS </a></span>
+        <span class="web-topic-3"> <a href="#">LINUX </a></span>
+        <span class="web-topic-4"><i class="fa fa-search fs"></i></span>
+      </div>
+
+      <div class="web-menu-icon">
+        <i class="fa fa-bars fa-2x"> </i>
+      </div>
+    </nav>
+    <?php
+    // echo phpinfo();
+    ?>
+  </header>
+  <br /><br /><br /><br /><br /><br />
+
+  <div class="content">
+    <div class="container">
+      <div class="banner">
+        <div class="row">
+          <div class="col-1">
+            <h1>
+              Learn to code
+              <br>
+              for free
+            </h1>
+            <p>Learn to code with our beginner-friendly tutorials and examples. Read tutorials, try examples, write programs, and learn to code.</p>
+            <p> Get All News,Tutorials and latest code updates</p>
+            <div class="banner-form">
+
+              <form method="post" action="#">
+                <input class="input-area" type="text" name="text-input" placeholder="enter mail ">
+                <input class="subscribe-fav" type="button" value="Subscribe">
+              </form>
+            </div>
+          </div>
+          <div class="col-2">
+            <img class="cl-img" src="icon/b-img.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card-container">
+    <div class="cards-area">
+      <div class="card-text choose-text">
+        <h1>Choose <br>what to learn</h1>
+        <p>Learn embedded <br>step by step.</p>
+      </div>
+      <a class="card-a" href="#">
+        <div class="card stl-1">
+          <div class="card-a-icon">
+            <object class="card-icon" width="30" height="30" type="image/svg+xml" data="/pro/icon/embed-c.svg"></object>
+          </div>
+          <h2 class="card-head"> Emed C</h2>
+          <div class="card-link"><span>Learn</span> <span>-></span></div>
+        </div>
+      </a>
+
+      <a class="card-a" href="soc/">
+        <div class="card">
+          <div class="card-a-icon">
+            <object class="card-icon" width="30" height="30" type="image/svg+xml" data="/pro/icon/processor.svg"></object>
+          </div>
+          <h2 class="card-head">SOC's</h2>
+          <div class="card-link"><span>Learn</span> <span>-></span></div>
+        </div>
+      </a>
+
+      <a class="card-a" href="#">
+        <div class="card stl-1">
+          <div class="card-a-icon">
+            <object class="card-icon" width="30" height="30" type="image/svg+xml" data="/pro/icon/rtos.svg"></object>
+          </div>
+          <h2 class="card-head"> RTOS</h2>
+          <div class="card-link"><span>Learn</span> <span>-></span></div>
+        </div>
+      </a>
+
+      <a class="card-a" href="#">
+        <div class="card stl2">
+          <div class="card-a-icon">
+            <object class="card-icon" width="30" height="30" type="image/svg+xml" data="/pro/icon/linux-1.svg"></object>
+          </div>
+          <h2 class="card-head"> Linux</h2>
+          <div class="card-link"><span>Learn</span> <span>-></span></div>
+        </div>
+      </a>
+
+      <a class="card-a" href="#">
+        <div class="card stl2">
+          <div class="card-a-icon">
+            <object class="card-icon" width="30" height="30" type="image/svg+xml" data="/pro/icon/processor.svg"></object>
+          </div>
+          <h2 class="card-head"> View All </h2>
+          <div class="card-link"><span>Explore</span> <span>-></span></div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="content-style">
+    <h2>Content Style/Type</h2>
+    <h4>What type of content you will get ?</h4>
+    <div class="ctype">
+      <div class="ctype-tab">
+        <a href="#BOOK1"><i class="fa fa-folder"></i> Documents</a>
+        <a href="#BOOK2"><i class="fa fa-folder"></i> Simulation</a>
+        <a href="#BOOK3"><i class="fa fa-folder"></i> Video Tutorial</a>
+        <a href="#BOOK4"><i class="fa fa-folder"></i> Design</a>
+        <a href="#BOOK5"><i class="fa fa-folder"></i> Code</a>
+        
+      </div>
+      <div class="ctype-disp">
+        <a class="b1" id="BOOK1" name="BOOK1">
+         <div class="book-link">
+           <h3>Documents</h3>
+           <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+         </div>
+        </a>
+
+        <a class="b1" id="BOOK2" name="BOOK2">
+         <div class="book-link">
+           <h3>Simulation</h3>
+           <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+         </div>
+        </a>
+
+        <a class="b1" id="BOOK3" name="BOOK3">
+         <div class="book-link">
+           <h3>Video Tutorial</h3>
+           <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+         </div>
+        </a>
+
+        <a class="b1" id="BOOK1" name="BOOK4">
+         <div class="book-link">
+           <h3>Design</h3>
+           <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+         </div>
+        </a>
+
+        <a class="b1" id="BOOK5" name="BOOK5">
+         <div class="book-link">
+           <h3>Code</h3>
+           <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+         </div>
+        </a>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="topic-area">
+    <a href="#" class="topic-link">
+      <div class="topic">
+        <h2>Why CodeSack ?</h2>
+        <div class="topic-ans">
+          <div class="answer">
+            <h3 class="head">Programming made easy</h3>
+            <p class="ans">
+              We focus on simplicity. Programming tutorials and examples written in simple, understandable language for beginners. 
+            </p>
+              <div class="foot"></div>
+          </div>
+
+          <div class="answer">
+            <h3 class="head">Open Source Content </h3>
+            <p class="ans">
+              A dedicated group of experts continually working to create programming resources that are accurate and easier to understand.
+            </p>
+              <div class="foot"></div>
+          </div>
+          <div class="answer">
+            <h3 class="head"> Learn by Doing</h3>
+            <p class="ans">
+              The only way to learn to program is by writing code. We provide a lot of complete examples so that run and edit code on your own. 
+            </p>
+              <div class="foot"></div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+
+<footer>
+<div class="foot-links">
+<div class="fa-col  fl-col-1">
+  <a href="#">SOC's</a>
+  <div class="foot-sub">
+    <a href="#">Mirco-controllers</a>
+    <a href="#">Ardiuno</a>
+    <a href="#">Raspberry Pi</a>
+  </div>
+</div>
+<div class="fa-col  fl-col-2">
+<a href="#">RTOS</a>
+  <div class="foot-sub">
+    <a href="#">Task Creation</a>
+    <a href="#">Task Control</a>
+    <a href="#">Task Utilities</a>
+    <a href="#">RTOS Kernel Control</a>
+    <a href="#">Direct To Task Notifications</a>
+    <a href="#">Queues</a>
+    <a href="#">Queue Sets</a>
+    <a href="#">Stream Buffers</a>
+    <a href="#">Message Buffers</a>
+    <a href="#">Semaphore / Mutexes</a>
+  </div>
+</div>
+<div class="fa-col  fl-col-3">
+<a href="#">LINUX</a>
+  <div class="foot-sub">
+   <a href="#">architecture</a>
+    <a href="#">Bash Command</a>
+    <a href="#">Bash Scripting</a>
+    <a href="#">Editors</a>
+    <a href="#">C/Cpp Compilers</a>
+    <a href="#">Makefile</a>
+    <a href="#">Kernel Programming</a>
+    <a href="#">Device Driver</a>
+    <a href="#">Semaphore / Mutexes</a>
+  </div>
+</div>
+
+<div class="fa-col fl-col-4">
+<a href="#">FOLLOW ME</a>
+  <div class="foot-sub">
+   <a href="#"><i class="fa fa-youtube"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <a href="#"><i class="fa fa-instagram"></i></a>
+  </div>
+</div>
+</div>
+<div class="cp-co">
+  <h5>All Rights Reserved</h5>
+  <p>@2020</p>
+</div>
+</footer>
+</body>
+</html>
---
+++ timestamp.php
@@ -0,0 +1,5 @@
+<?php
+    date_default_timezone_set('Asia/Kolkata');
+    $timestamp = date('H:i:s');
+    echo $timestamp;
+ ?>
